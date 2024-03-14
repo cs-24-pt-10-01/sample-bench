03a3f7c43c99ec7e608d50a1f753066be35d446b
Jakob <31919330+Jakobzs@users.noreply.github.com>
2024-03-14 10:27:45 +0100

    Update fibhead.c

--- fibhead.c
+++ fibhead.c
@@ -8,11 +8,9 @@ int a = 0;
 
 // test method (our own implementation)
 unsigned int fibb(unsigned int a){
-    start_rapl("fibb");
     if (a <= 1){
         return a;
     }
-    stop_rapl("fibb");
     return fibb(a-1) + fibb(a-2);
 }
 
@@ -22,9 +20,7 @@ int main(int argc, char *argv[]) {
     // Start benchmarking how long the fibb method takes
     int start = clock();
 
-    start_rapl("main");
     unsigned int result = fibb(fibParam);
-    stop_rapl("main");
 
     // End benchmarking how long the fibb method takes
     int end = clock();
@@ -41,10 +37,12 @@ int main(int argc, char *argv[]) {
 
 void __attribute__((no_instrument_function)) __cyg_profile_func_enter(void *func,  void *caller)
 {
-  printf("enter %p\n", func);
+  start_rapl("enter");
+  //printf("enter %p\n", func);
 }
 
 void __attribute__((no_instrument_function)) __cyg_profile_func_exit (void *func,  void *caller)
 {
-  printf("exit %p\n", func);
+  stop_rapl("exit");
+  //printf("exit %p\n", func);
 }
